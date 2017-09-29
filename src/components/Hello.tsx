3c6164d00a911655b6f169422e4446939b362780
Roman Frołow <rofrol@gmail.com>
2017-09-29 17:39:37 +0200

    Some changes

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,7 +1,30 @@
 import * as React from "react";
 
-export interface HelloProps { compiler: string; framework: string; }
-
 import './Hello.scss';
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+function Hello({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import Hello from "./components/Hello";
 
 import './style.scss';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    <Hello />,
+    document.getElementById("root")
 );
